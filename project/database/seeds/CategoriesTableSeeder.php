d6b7ce5884e5119a93304a241924782e1c653606
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-04-16 01:10:51 -0300

    better

--- project/database/seeds/CategoriesTableSeeder.php
+++ project/database/seeds/CategoriesTableSeeder.php
@@ -6,37 +6,42 @@ use Illuminate\Database\Seeder;
 class CategoriesTableSeeder extends Seeder
 {
     /**
+     * Default categories;
+     *
+     * @var array
+     */
+    protected $categories = [
+        [
+            'name' => 'Car',
+            'icon' => 'fa-car',
+        ],
+        [
+            'name' => 'Motorcycle',
+            'icon' => 'fa-motorcycle',
+        ],
+        [
+            'name' => 'Property to rent',
+            'icon' => 'fa-building',
+        ],
+        [
+            'name' => 'Electronics',
+            'icon' => 'fa-television',
+        ],
+        [
+            'name' => 'To your home',
+            'icon' => 'fa-television',
+        ],
+    ];
+
+    /**
      * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $categories = [
-            [
-                'name' => 'Car',
-                'icon' => 'fa-car',
-            ],
-            [
-                'name' => 'Motorcycle',
-                'icon' => 'fa-motorcycle',
-            ],
-            [
-                'name' => 'Property to rent',
-                'icon' => 'fa-building',
-            ],
-            [
-                'name' => 'Electronics',
-                'icon' => 'fa-television',
-            ],
-            [
-                'name' => 'To your home',
-                'icon' => 'fa-television',
-            ],
-        ];
-
-        collect($categories)->each(function (array $category) {
+        foreach ($this->categories as $category) {
             Category::create($category);
-        });
+        }
     }
 }
