8686af16d49dd95a7caed12b505a9c6dbf3c7191
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-04-18 20:57:21 -0300

    add tailwind

--- project/webpack.mix.js
+++ project/webpack.mix.js
@@ -1,4 +1,12 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/scss/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js');
+
+mix.sass('resources/assets/scss/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [
+      tailwindcss('./tailwind.js'),
+    ],
+  });
