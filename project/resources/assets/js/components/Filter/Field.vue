31ecba7d688f2b1e421664464c36563930ee8b84
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-04-28 02:08:51 -0300

    category list

--- project/resources/assets/js/components/Filter/Field.vue
+++ project/resources/assets/js/components/Filter/Field.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="relative flex mb-10" :class="{ 'flex-col': ! row }">
-    <h4 class="font-medium text-grey-darkest block text-xs border-b pb-2 mb-6" v-text="title.toUpperCase()" v-if="title"></h4>
+  <div class="relative flex mb-8" :class="{ 'flex-col': ! row }">
+    <h4 class="font-medium text-grey-darkest block text-xs border-b pb-2 mb-4" v-text="title.toUpperCase()" v-if="title"></h4>
     <slot />
   </div>
 </template>
--- project/resources/assets/js/components/Filter/Main.vue
+++ project/resources/assets/js/components/Filter/Main.vue
@@ -2,14 +2,23 @@
   <aside>
     <form>
       <filter-field class="h-8 border rounded-full items-center pl-4 pr-8" row>
-        <input type="text" placeholder="Search" class="rounded-full flex-1">
+        <input type="text" placeholder="Search" class="rounded-full w-full">
         <button type="submit" class="-mr-4">
           <i class="fa fa-search text-grey"></i>
         </button>
       </filter-field>
-      <filter-field title="Cars">
-      </filter-field>
     </form>
+
+    <filter-field title="Categories">
+      <ul class="list-reset text-sm">
+        <li v-for="i in 8">
+          <router-link to="/" class="text-grey-darkest block p-1 -mx-1 my-1 hover:bg-grey-lightest rounded">
+            <span class="float-right text-grey">255</span>
+            Cars
+          </router-link>
+        </li>
+      </ul>
+    </filter-field>
   </aside>
 </template>
 
