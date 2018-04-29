e4b9801691dacca04ad07ec525ac0bf04ea20f3f
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-04-28 23:58:20 -0300

    custom parser

--- project/resources/assets/js/core/router.js
+++ project/resources/assets/js/core/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { stringify, parse } from 'qs';
 
 import Home from '@/Home/Main.vue';
 import Filter from '@/Filter/Main.vue';
@@ -7,12 +8,18 @@ import Filter from '@/Filter/Main.vue';
 Vue.use(VueRouter);
 
 export default new VueRouter({
+  mode: 'history',
+  parseQuery: parse,
+  stringifyQuery: query => stringify(query, { arrayFormat: 'brackets', encode: false }),
   routes: [
     {
-      path: '/',
+      path: '/:categorySlug?',
       components: {
         default: Home,
         sidebar: Filter,
+      },
+      props: {
+        sidebar: true,
       },
       name: 'home.index',
     },
