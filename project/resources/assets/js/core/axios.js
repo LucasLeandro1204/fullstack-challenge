f2d9edd698453b02082eac6a8818590c44704acb
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-05-01 19:45:39 -0300

    use qs here too

--- project/resources/assets/js/core/axios.js
+++ project/resources/assets/js/core/axios.js
@@ -1,5 +1,7 @@
 import Axios from 'axios';
+import { stringify, parse } from 'qs';
 
 export default Axios.create({
   baseURL: '/api/',
+  paramsSerializer: query => stringify(query, { arrayFormat: 'brackets', encode: false, addQueryPrefix: true }),
 });
