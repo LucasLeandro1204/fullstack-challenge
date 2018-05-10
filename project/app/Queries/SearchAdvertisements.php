70c7f191544d26811eaa5fd216728671b2d91a5f
LucasLeandro1204 <lucasleandro1204@gmail.com>
2018-05-09 21:39:30 -0300

    waaayy better

--- project/app/Queries/SearchAdvertisements.php
+++ project/app/Queries/SearchAdvertisements.php
@@ -2,116 +2,108 @@
 
 namespace App\Queries;
 
+use App\Core\Query;
 use App\Advertisement;
-use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Contracts\Pagination\Paginator;
 
-class SearchAdvertisements
+class SearchAdvertisements extends Query
 {
     /**
-     * Query
-     *
-     * @var string
+     * {@inheritdoc}
      */
-    protected $query;
+    protected $defaults = [
+        'order_by' => [
+            'field' => 'id',
+            'sort' => 'desc',
+        ],
+    ];
 
     /**
-     * Category id
-     *
-     * @var integer
+     * {@inheritdoc}
      */
-    protected $category;
-
-    /**
-     * Sort field.
-     *
-     * @var string
-     */
-    protected $field;
-
-    /**
-     * Sort order.
-     *
-     * @var string
-     */
-    protected $sort;
-
-    /**
-     * Filters.
-     *
-     * @var array
-     */
-    protected $filters;
+    protected $rules = [
+        'search' => 'string',
+        'filters' => 'array',
+        'filters.*' => 'array',
+        'category' => 'integer',
+        'filters.*.to' => 'integer',
+        'filters.*.from' => 'integer',
+        'order_by.field' => 'sometimes|required',
+        'order_by.sort' => 'sometimes|required|string|in:asc,desc',
+    ];
 
     /**
      * Create a new instance.
      */
-    public function __construct(?string $query, ?integer $category, string $field, string $sort, array $filters)
+    public function __construct(Advertisement $model)
     {
-        $this->query = $query;
-        $this->category = $category;
-        $this->field = $field;
-        $this->sort = $sort;
-        $this->filters = $filters;
+        $this->model = $model;
     }
 
     /**
-     * Get the advertisements filtered and paginated.
+     * Get paginated.
      */
     public function get(int $perPage = 15): Paginator
     {
-        $query = Advertisement::orderBy($this->field, $this->sort)
-            ->when($this->query, function (Builder $builder) {
-                $builder->where('title', 'LIKE', "%$this->query%");
-            })
-            ->when($this->category, function (Builder $builder) {
-                $builder->whereHas('category', function (Builder $builder) {
-                    $builder->where('id', $this->category);
-                });
-            })
-            ->when($this->filters, function (Builder $builder) {
-                foreach ($this->filters as $key => $value) {
-                    $builder->whereHas('values', function (Builder $builder) use ($key, $value) {
-                        $builder->where('field_id', $key);
-
-                        if (isset($value['from']) || isset($value['to'])) {
-                            $builder->where('value', '>', $value['from'] ?? 0)
-                                ->when(isset($value['to']), function (Builder $builder) use ($value) {
-                                    $builder->where('value', '<', $value['to']);
-                                });
-                        } else {
-                            $builder->whereIn('value', $value);
-                        }
-                    });
-                }
-            });
-
-        return $query->paginate();
+        return $this->model->paginate($perPage);
     }
 
     /**
-     * Create a new query instance from request.
+     * Order by method proxy.
      */
-    public static function from(Request $request): self
+    protected function orderBy(array $config): self
     {
-        $request->validate([
-            'query' => 'string',
-            'filters' => 'array',
-            'filters.*' => 'array',
-            'category' => 'integer',
-            'filters.*.to' => 'integer',
-            'filters.*.from' => 'integer',
-            'order_by.field' => 'sometimes|required',
-            'order_by.sort' => 'sometimes|required|string|in:asc,desc',
-        ]);
+        $this->model->orderBy($config['field'], $config['sort']);
 
-        return new self(
-            $request->get('query'),
-            $request->get('category'),
-            $request->get('order_by.field', 'id'),
-            $request->input('order_by.sort', 'desc'),
-            $request->input('filters', [])
-        );
+        return $this;
+    }
+
+    /**
+     * Filter by category.
+     */
+    public function category(int $id): self
+    {
+        $this->model->whereHas('category', function (Builder $builder) use ($id) {
+            $builder->where('id', $id);
+        });
+
+        return $this;
+    }
+
+    /**
+     * Custom fields filter.
+     */
+    public function filters(array $filters): self
+    {
+        foreach ($filters as $key => $value) {
+            $builder->whereHas('values', function (Builder $builder) use ($key, $value) {
+                $builder->where('field_id', $key);
+
+                if (isset($value['from']) || isset($value['to'])) {
+                    $builder->where('value', '>', $value['from'] ?? 0)
+                        ->when(isset($value['to']), function (Builder $builder) use ($value) {
+                            $builder->where('value', '<', $value['to']);
+                        });
+                } else {
+                    $builder->whereIn('value', $value);
+                }
+            });
+        }
+
+        return $this;
+    }
+
+    /**
+     * Filter by title or description.
+     */
+    public function search(string $keyword): self
+    {
+        $this->model->where(function (Builder $builder) use ($keyword) {
+            $builder->where('title', 'LIKE', "%$keyword%")
+                ->orWhere('description', 'LIKE', "%$keyword%");
+        });
+
+        return $this;
     }
 }
